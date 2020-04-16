7ad592b9d6b612c55792fe2ecf6bda113da6d7a9
scott88lee <scott88lee@gmail.com>
2020-04-17 00:26:17 +0800

    Set up handlebars rendering.

--- README.md
+++ README.md
@@ -1,20 +1,26 @@
 # Express-MVC
-
 npm install
 node index.js to start
 
+Project structure
 Express-MVC
-├── controllers
-├── model
-├── views
+├── /controllers
+├── /model
+├── /views
 │   ├── /layouts
 │   ├── /partials
 │   ├── error.hbs
 │   ├── index.hbs
 │   └── login.hbs
-├── app.js
+├── /routes
+├── index.js
+├── db.js
 ├── package.json
-├── app.js
-├── package.json
-├── app.js
-└── package.json
+└── README.md
+
+#Developer reference
+Handlebars:
+Very good
+https://medium.com/@waelyasmina/a-guide-into-using-handlebars-with-your-express-js-application-22b944443b65
+Not so good
+https://hackersandslackers.com/handlebars-templates-expressjs/
--- index.js
+++ index.js
@@ -1,19 +1,49 @@
 // IMPORTS
 const express = require('express');
+const handlebars = require('express-handlebars');
+//const cookieParser = require('cookie-parser');
+
 const app = express();
 
 // Public Folder and Middleware
 app.use(express.static('public'));
+//app.use(cookieParser());
+
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// View engine setup
+const hbsConfig = {
+    layoutsDir: __dirname + '/views/layouts',
+    extname: 'hbs',
+    defaultLayout: 'defaultLayout',
+}
+app.set('view engine', 'hbs');
+app.engine('hbs', handlebars(hbsConfig));
+
 // ROUTES
+// var indexRouter = require('./routes/index');
+// var productsRouter = require('./routes/products');
+// app.use('/', indexRouter);
+// app.use('/products', productsRouter);
+
+app.get('/', (req, res) => {
+    //Serves the body of the page aka "main.handlebars"
+    // to the container //aka "index.handlebars"
+    res.render('main', { layout: 'index' });
+});
+
+app.get('/weaser', (req, res) => {
+    //Serves the body of the page aka "main.handlebars"
+    // to the container //aka "index.handlebars"
+    res.render('main', { yay: "helloWorld"});
+});
 
 //404 Precessing
-app.get('*', (req, res) => { 
-    res.statusCode = 404;
-    let dude = {msg:'Hello World'}
-    res.send(dude);
+app.get('*', (req, res) => {
+    crap = { yay: 'Hello World' }
+    res.render('err404', crap);
 });
 
 // LISTEN
-app.listen(3000, () => {console.log('Listen port: 3000')} );
+app.listen(3000, () => { console.log('Listen port: 3000') });
