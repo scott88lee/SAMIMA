1aab15c2cd13d0678fa2d1bc3c622f265e52564b
scott88lee <scott88lee@gmail.com>
2020-04-18 19:22:58 +0800

    Cleaned up Products SQL

--- tools/seed.sql
+++ tools/seed.sql
@@ -1,52 +1,52 @@
-DROP TABLE IF EXIST products;
-DROP TABLE IF EXIST users;
-DROP TABLE IF EXIST inventory;
-DROP TABLE IF EXIST suppliers;
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS inventory;
+DROP TABLE IF EXISTS suppliers;
 
 CREATE TABLE IF NOT EXISTS users (
     user_id SERIAL PRIMARY KEY,
     name TEXT NOT NULL,
     pwd_hash TEXT NOT NULL,
-    role TEXT NOT NULL
+    user_role TEXT NOT NULL
 );
 
+DROP TABLE IF EXISTS products;
 CREATE TABLE IF NOT EXISTS products (
     product_id SERIAL PRIMARY KEY,
     SKU VARCHAR(30) NOT NULL,
-    brand INT NOT NULL,
+    brand TEXT NOT NULL,
     model TEXT NOT NULL,
-    description TEXT,
+    product_desc TEXT,
     msrp NUMERIC(5, 2),
     map NUMERIC(5, 2),
     physical_item BOOLEAN
 );
 
+INSERT INTO products (SKU, brand, model, product_desc, msrp, map, physical_item)
+VALUES ('YMAF310NT', 'Yamaha', 'F310 NT', 'F310 Acoustic guitar Natural', 229, 215, TRUE);
+
+INSERT INTO products (SKU, brand, model, product_desc, msrp, map, physical_item)
+VALUES ('YMAF310TBS', 'Yamaha', 'F310 TBS', 'F310 Acoustic guitar Sunburst', 229, 215, TRUE);
+
+INSERT INTO products (SKU, brand, model, product_desc, msrp, map, physical_item)
+VALUES ('UEN3123', 'Orange', 'Crush 20', '20W Electric guitar amp', 159, 149, TRUE);
+
+INSERT INTO products (SKU, brand, model, product_desc, msrp, map, physical_item)
+VALUES ('UEN1231', 'Zoom', 'G3Xn', 'G3Xn Multi-effects Processor', 280, 280, TRUE);
+
+INSERT INTO products (SKU, brand, model, product_desc, msrp, map, physical_item)
+VALUES ('UEN1234', 'Zoom', 'G1X Four', 'G1X Four Multi-effects Processor', 140, 140, TRUE);
+
+INSERT INTO products (SKU, brand, model, product_desc, msrp, map, physical_item)
+VALUES ('SVC001', 'Service', 'Restring and Tune', 'Resting and Tune service', 15, 10, FALSE);
+
+INSERT INTO products (SKU, brand, model, product_desc, msrp, map, physical_item)
+VALUES ('SVC002', 'Service', 'Pickup wiring', 'Guitar pickup wiring', 40, 30, FALSE);
+
+
 CREATE TABLE IF NOT EXISTS inventory (
     product_id INT NOT NULL,
     product_quantity INT NOT NULL
 );
-
-
-INSERT INTO products (SKU, brand, model, description, msrp, map)
-VALUES ('YMAF310NT', 'Yamaha', 'F310 NT', 'F310 Acoustic guitar Natural', 229', 215, TRUE);
-
-INSERT INTO products (SKU, brand, model, description, msrp, map)
-VALUES ('YMAF310TBS', 'Yamaha', 'F310 TBS', 'F310 Acoustic guitar Sunburst', 229', 215, TRUE);
-
-INSERT INTO products (SKU, brand, model, description, msrp, map)
-VALUES ('UEN3123', 'Orange, 'Crush 20', '20W Electric guitar amp', 159, 149, TRUE);
-
-INSERT INTO products (SKU, brand, model, description, msrp, map)
-VALUES ('UEN1231', 'Zoom', 'G3Xn', 'G3Xn Multi-effects Processor', 280, 280, TRUE);
-
-INSERT INTO products (SKU, brand, model, description, msrp, map)
-VALUES ('UEN1234', 'Zoom', 'G1X Four', 'G1X Four Multi-effects Processor', 140, 140, TRUE);
-
-INSERT INTO products (SKU, brand, model, description, msrp, map)
-VALUES ('SVC001', 'Service', 'Restring and Tune', 'Resting and Tune service', 15, 10, FALSE);
-
-INSERT INTO products (SKU, brand, model, description, msrp, map)
-VALUES ('SVC002', 'Service', 'Pickup wiring', 'Guitar pickup wiring', 40, 30, FALSE);
 
 CREATE TABLE IF NOT EXISTS purchases (
     purchase_id SERIAL PRIMARY KEY,
