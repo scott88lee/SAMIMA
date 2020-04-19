787027f738f2b4c381132548449775597e0b9a3f
scott88lee <scott88lee@gmail.com>
2020-04-19 17:26:32 +0800

    frontend product search

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,1 +1,21 @@
 $('.ui.dropdown').dropdown();
+
+function searchTable() {
+    const input = document.getElementById("searchInput");
+    const filter = input.value.toUpperCase();
+    const table = document.getElementById("productTable");
+    const tr = table.getElementsByTagName("tr");
+
+    for (let i = 0; i < tr.length; i++) {
+        col1 = tr[i].getElementsByTagName("td")[0].textContent || tr[i].getElementsByTagName("td")[0].innerText;
+        col2 = tr[i].getElementsByTagName("td")[1].textContent || tr[i].getElementsByTagName("td")[1].innerText;
+        col3 = tr[i].getElementsByTagName("td")[2].textContent || tr[i].getElementsByTagName("td")[2].innerText;
+        col4 = tr[i].getElementsByTagName("td")[3].textContent || tr[i].getElementsByTagName("td")[3].innerText;
+        
+        if (col1.toUpperCase().indexOf(filter) > -1 || col2.toUpperCase().indexOf(filter) > -1 || col3.toUpperCase().indexOf(filter) > -1 || col4.toUpperCase().indexOf(filter) > -1) {
+            tr[i].style.display = "";
+        } else {
+            tr[i].style.display = "none";
+        }
+    }
+}
