931ea4467ca020e68a8d589bb5eb6ce50a874d9b
scott88lee <scott88lee@gmail.com>
2020-06-08 11:28:52 +0800

    cats

--- models/products.js
+++ models/products.js
@@ -23,7 +23,7 @@ module.exports = {
 
 	addProduct: (prod) => {
 		return new Promise((resolve, reject) => {
-			const queryString = "INSERT INTO products (SKU, brand, model, product_desc, msrp, map, physical_item) VALUES ('" + prod.sku + "', '" + prod.brand + "', '" + prod.model + "', '" + prod.product_desc + "', " + prod.msrp + ", " + prod.map + ", " + prod.physical_item + ");"
+			const queryString = "INSERT INTO products (SKU, brand, model, cat, product_desc, msrp, map, physical_item, deprecated) VALUES ('" + prod.sku + "', '" + prod.brand + "', '" + prod.model + "', '" + prod.cat + "', '" + prod.product_desc + "', " + prod.msrp + ", " + prod.map + ", " + prod.physical_item + ", " + false + ");"
 			console.log(queryString);
 			
 			db.query(queryString, (err, result) => {
@@ -57,7 +57,7 @@ module.exports = {
 	
 	updateProduct: (product) => {
 		return new Promise((resolve, reject) => {
-			const queryString = "UPDATE products SET SKU='" + product.sku + "', brand='" + product.brand + "', model='" + product.model + "', product_desc='" + product.product_desc + "', msrp=" + product.msrp + ", map=" + product.map + ", physical_item=" + product.physical_item + ", deprecated=" + product.deprecated + " WHERE product_id='" + product.product_id + "' RETURNING brand, model;"
+			const queryString = "UPDATE products SET SKU='" + product.sku + "', brand='" + product.brand + "', model='" + product.model + "', cat='" + product.cat + "', product_desc='" + product.product_desc + "', msrp=" + product.msrp + ", map=" + product.map + ", physical_item=" + product.physical_item + ", deprecated=" + product.deprecated + " WHERE product_id='" + product.product_id + "' RETURNING brand, model;"
 			console.log(queryString);
 			
 			db.query(queryString, (err, result) => {
