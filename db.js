acba8deab6917faca46e38287563c2c4ed7ab086
scott88lee <scott88lee@gmail.com>
2020-06-17 19:49:57 +0800

    Cloud-Dev config

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const pg = require('pg');
 
-const config = {
+const local = {
   user: 'postgres',
   host: '127.0.0.1',
   database: 'sam-dev', //soundalchemymusic inventory management accounting
@@ -15,7 +15,15 @@ const awsRDS = {
   port: 5432
 };
 
-const pool = new pg.Pool( process.env.NODE_ENV == 'dev' ? config : awsRDS);
+const dev = {
+  user: 'master',
+  password: 'testing123',
+  host: 'sam-dev.cf6awl3jeoo9.ap-southeast-1.rds.amazonaws.com',
+  database: 'samdev', //AWS RDS config
+  port: 5432
+};
+
+const pool = new pg.Pool( process.env.NODE_ENV == 'dev' ? dev : awsRDS);
 
 pool.on('error', function (err) {
   console.log('idle client error', err.message, err.stack);
